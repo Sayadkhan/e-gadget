78d974ae156c988ea54bf2b4583c20c41f15d1c3
sayadkhan <abdullahalsayad03@gmail.coml>
2023-01-09 16:41:05 +0600

    card section done

---
+++ src/Components/Card.js
@@ -0,0 +1,32 @@
+import { currencyFormatter } from "../utilities/currencyFormatter";
+
+const Card = ({ product }) => {
+  return (
+    <div className="product flex flex-col gap-2 rounded-xl bg-white shadow-md overflow-hidden hover:shadow-2xl duration-300">
+      <div className="img">
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div className="texts flex flex-col gap-2 px-6 pb-5">
+        <span className="category-tag uppercase text-xs font-semibold tracking-widest text-orange-500">
+          {product.category}
+        </span>
+        <h3 className="title text-xl font-medium h-[5.25rem]">
+          {product.name}
+        </h3>
+        <p className="details text-gray-500 h-[6.5rem]">
+          {product.description}
+        </p>
+        <div className="flex justify-between items-center">
+          <span className="price text-xl font-medium text-sky-500">
+            {currencyFormatter(product.price)}
+          </span>
+          <button className="cart-btn uppercase bg-violet-500 text-violet-50 font-medium py-3 px-8 rounded-md hover:bg-red-500 hover:text-orange-50 duration-300 shadow-violet-300 shadow-lg hover:shadow-orange-300">
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
